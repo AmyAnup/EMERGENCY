198ccb5a0eb531859515138b350fecc5e9cc5679
Aneena Sebastian <aneenasebastian2028@cs.sjcetpalai.ac.in>
2025-09-26 21:57:37 +0530

    Fix line endings and add ImageWithFallback component

--- components/figma/ImageWithFallback.tsx
+++ components/figma/ImageWithFallback.tsx
@@ -7,10 +7,12 @@ export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElemen
   const [didError, setDidError] = useState(false)
 
   const handleError = () => {
-    setDidError(true)
+    if (props.src !== ERROR_IMG_SRC) {
+      setDidError(true)
+    }
   }
 
-  const { src, alt, style, className, ...rest } = props
+  const { src, alt = 'Image', style, className, ...rest } = props
 
   return didError ? (
     <div
@@ -18,7 +20,7 @@ export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElemen
       style={style}
     >
       <div className="flex items-center justify-center w-full h-full">
-        <img src={ERROR_IMG_SRC} alt="Error loading image" {...rest} data-original-url={src} />
+        <img src={ERROR_IMG_SRC} alt="Error loading image" {...rest} />
       </div>
     </div>
   ) : (
